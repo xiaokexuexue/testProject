1cb0a4d5eeece30193fc4e9fe94f1657f5501fba
xiaokexuexue <2373271726@qq.com>
2019-10-25 00:52:29 +0800

    111

--- src/com/xsh/test/Demo.java
+++ src/com/xsh/test/Demo.java
@@ -1,8 +1,10 @@
 package com.xsh.test;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @auther: xueshh
@@ -20,5 +22,13 @@ public class Demo {
         Date time1 = calendar.getTime();
         String format = sdf.format(time1);
         System.out.println(format);
+
+        List<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
